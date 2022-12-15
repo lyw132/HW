a91a7f5d33563a0c47bb0c44f75d18e221a3292e
lyw132 <lyw_132@163.com>
2022-12-15 21:53:41 +0800

    上传

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,38 @@
+const zc = document.querySelector(".header_zc");
+const ss = document.querySelector(".header_ss");
+const dl = document.querySelector(".header_dl");
+const xz = document.querySelector(".app_xz_box");
+const dl_cover = document.querySelector(".header_zc_show");
+const deleter = document.querySelector(".example_delete");
 
+const market_xz = document.querySelector(".market_xz");
+
+// 搜索
+zc.addEventListener("click", function () {
+  ss.classList.remove("cover");
+});
+deleter.addEventListener("click", () => {
+  ss.classList.add("cover");
+});
+// 登录
+dl.addEventListener("mouseover", function () {
+  dl_cover.classList.remove("cover");
+
+  setTimeout(() => {
+    dl.addEventListener("mouseout", () => {
+      dl_cover.classList.add("cover");
+      // timerld = !timerld;
+    });
+  }, 1000);
+});
+
+xz.addEventListener("mouseover", () => {
+  console.log(1);
+  market_xz.classList.remove("cover");
+  xz.addEventListener("mouseout", () => {
+    setTimeout(() => {
+      market_xz.classList.add("cover");
+      console.log(2);
+    }, 1000);
+  });
+});
